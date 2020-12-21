7fcac16dc9cfbca24bab90c69bf4956279f958ca
굿닥 <pptrollDev@gmail.com>
2020-12-21 16:49:03 +0900

    add eslint, prettier, husky, lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import "./style.scss";
+import './style.scss';
 
 export default function App() {
-    const alert = (msg: any) => window.alert(msg);
-    return <div onClick={alert}>React + TypeScript + Webpack!</div>;
+  return <div>React Stater!</div>;
 }
