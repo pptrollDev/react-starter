16b9d885ccdf05cf91baf6450d6279b0216ba023
굿닥 <pptrollDev@gmail.com>
2020-12-22 13:46:58 +0900

    add webpack mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,66 +2,70 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = {
-    entry: {
-        app: path.join(__dirname, 'src', 'index.tsx'),
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?/,
-                exclude: /node_module/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
-                        plugins: ['@babel/proposal-class-properties'],
+module.exports = (env, options) =>{
+    return {
+        mode: options.mode,
+        entry: {
+            app: path.join(__dirname, 'src', 'index.tsx'),
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?/,
+                    exclude: /node_module/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react'],
+                            plugins: ['@babel/proposal-class-properties'],
+                        },
                     },
                 },
-            },
-            {
-                test: /\.(ts|tsx)$/,
-                use: ['ts-loader'],
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    "style-loader",
-                    // Translates CSS into CommonJS
-                    "css-loader",
-                    // Compiles Sass to CSS
-                    "sass-loader",
-                ],
-            },
+                {
+                    test: /\.(ts|tsx)$/,
+                    use: ['ts-loader'],
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        "style-loader",
+                        // Translates CSS into CommonJS
+                        "css-loader",
+                        // Compiles Sass to CSS
+                        "sass-loader",
+                    ],
+                },
+            ],
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'build'),
+        },
+        devServer: {
+            contentBase: path.join(__dirname, "./build/"),
+            port: 8000
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+                filename: "./index.html",
+                templateParameters: {
+                    env: options.mode === 'production' ? '' : 'dev',
+                },
+                minify:
+                    options.mode === 'production'
+                        ? {
+                            collapseWhitespace: true,
+                            removeComments: true,
+                        }
+                        : false,
+            }),
+            new BundleAnalyzerPlugin()
         ],
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "./dist/"),
-        port: 8000
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            templateParameters: {
-                env: process.env.NODE_ENV === 'production' ? '' : '[DEV]',
-            },
-            minify:
-                process.env.NODE_ENV === 'production'
-                    ? {
-                        collapseWhitespace: true,
-                        removeComments: true,
-                    }
-                    : false,
-        }),
-        new BundleAnalyzerPlugin()
-    ],
-};
+    };
+}
