9664303575e3b6b489f7110ae6e61d9445f0c3a8
박성찬(Chan) <chan.park@gmail.com>
2020-12-27 19:40:32 +0900

    add route code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ export default function App() {
       <Suspense fallback={<div>Loading...</div>}>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route path="/Home" component={Home} />
           <Route path="/about" component={About} />
         </Switch>
       </Suspense>
