7e9a91e9db953056223477c2799dfaad708d5038
박성찬(Chan) <chan.park@gmail.com>
2020-12-27 15:53:20 +0900

    code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import './style.scss';
 
 export default function App() {
-  return <div>React Starter!</div>;
+  const Home = lazy(() => import('./pages/Home'));
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Home />
+    </Suspense>
+  );
 }
