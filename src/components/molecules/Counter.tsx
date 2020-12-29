4b7987e50138cfeb79b90470aca6153b522a6458
박성찬(Chan) <chan.park@gmail.com>
2020-12-29 12:15:08 +0900

    fix lint

--- src/components/molecules/Counter.tsx
+++ src/components/molecules/Counter.tsx
@@ -11,9 +11,16 @@ function Counter({ count, onIncrease, onDecrease, onIncreaseBy }: CountProps) {
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick={onIncrease}>+1</button>
-      <button onClick={onDecrease}>-1</button>
-      <button onClick={() => onIncreaseBy(5)}>+5</button>
+      <button type="button" onClick={onIncrease}>
+        +1
+      </button>
+      <button type="button" onClick={onDecrease}>
+        -1
+      </button>
+
+      <button type="button" onClick={() => onIncreaseBy(5)}>
+        +5
+      </button>
     </div>
   );
 }
